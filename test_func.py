aec5957379b208b1ab36a771517d1b20d9c66166
ALEX NEVES <alexprogran@gmail.com>
2023-03-10 10:55:59 -0300

    Correção de teste

---
+++ test_func.py
@@ -0,0 +1,30 @@
+
+
+"""
+OLÁ PROFESSOR, NÃO CONSIGO GERAR UM TESTE
+"POSITIVO" MESMO ISERINDO OS VALORES CORRETOS NA FUNÇÃO, APONTA O MEU ERRO, OBRIGADO.
+"""
+'''
+NÃO ACISTIR A TUA AULA AO VIVO POIS A MESMA 
+SÓ FICA DISPONÍVEL APÓS EVIARMOS O DESAFIO DA SEMANA
+'''
+def teste(valor_unitario, quantidade):
+  
+    if quantidade >= 10 and quantidade <= 99:
+        desconto = 0.95
+    elif quantidade >= 100 and quantidade <= 999:
+        desconto = 0.90
+    elif quantidade >= 1000:
+        desconto = 0.85
+
+    # No desconto o completo de até 1 é o valor do desconto.
+    # Ex: 0,85 tem 15% de desconto. -> 1 - 0,85 = 0.15
+
+    return valor_unitario * desconto * quantidade
+  
+
+def test_teste():
+        
+    assert teste(100,10) == 950
+   
+    
